7166ad8b880a1faf519c5d880bfb6b6f4b5f1103
Gonr <one642077@gmail.com>
2023-05-13 19:36:06 +0300

    1.1.5 small fix

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,16 +1,18 @@
 import { onAuthStateChanged } from 'firebase/auth'
 import { createContext, useEffect, useState } from 'react'
 import { auth } from '../firebase'
+import Loading from '../pages/Loading'
 
 export const AuthContext = createContext()
 
 export const AuthContextProvider = ({ children }) => {
 	const [currentUser, setCurrentUser] = useState({})
+	const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
 		const un = onAuthStateChanged(auth, user => {
 			setCurrentUser(user)
-			console.log(user)
+			setLoading(false)
 		})
 
 		return () => {
@@ -20,7 +22,7 @@ export const AuthContextProvider = ({ children }) => {
 
 	return (
 		<AuthContext.Provider value={{ currentUser }}>
-			{children}
+			{loading ? <Loading /> : children}
 		</AuthContext.Provider>
 	)
 }
---
+++ src/pages/Loading.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Loading() {
+	return <div className='Loading'>Loading...</div>
+}
+
+export default Loading
